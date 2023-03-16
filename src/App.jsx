92bfc39be78ac03ca694216c2da375a0d7f4c5a4
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-03-16 12:58:06 +0200

    feat: add page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
+import JokeCard from "./components/JokeCard";
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      setup: "What's the best thing about a Boolean?",
+      punchline: "Even if you're wrong, you're only off by a bit",
+    },
+    {
+      id: 2,
+      setup: "Why do programmers wear glasses?",
+      punchline: "Because they need to C#",
+    },
+  ];
   return (
-    <div className="App">
-      <h1>react introductory exercises</h1>
+    <div className="page-container">
+      <div className="cards">
+        {data.map((joke, index) => (
+          <JokeCard key={index} {...joke} />
+        ))}
+      </div>
     </div>
   );
 }
