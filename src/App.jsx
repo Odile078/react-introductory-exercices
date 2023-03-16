091f2a066ea71224c86591a00cdb23bd54f47b60
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-03-16 18:21:03 +0200

    feat: add a data folder for jokes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import "./App.css";
 import JokeCard from "./components/JokeCard";
-
+import { jokes } from "./data/jokes";
 function App() {
-  const data = [
-    {
-      id: 1,
-      setup: "What's the best thing about a Boolean?",
-      punchline: "Even if you're wrong, you're only off by a bit",
-    },
-    {
-      id: 2,
-      setup: "Why do programmers wear glasses?",
-      punchline: "Because they need to C#",
-    },
-  ];
   return (
     <div className="page-container">
       <div className="cards">
-        {data.map((joke, index) => (
+        {jokes.map((joke, index) => (
           <JokeCard key={index} {...joke} />
         ))}
       </div>
