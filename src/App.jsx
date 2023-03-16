ce1d656510444ab188e5a61d44ad82d002283647
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-03-16 12:34:07 +0200

    feat: add page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,82 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const handleFormSubmit = () => {
+    alert(`Hello ${firstName} ${lastName} !`);
+  };
   return (
-    <div className="App">
-      <h1>react introductory exercises</h1>
+    <div className="page-container">
+      <div className="nav-container">
+        <ul className="nav-list">
+          {[
+            { name: "home", url: "https://clever-naiad-78d51b.netlify.app/" },
+            {
+              name: "exercise1",
+              url: "https://exercise1-hello-world--clever-naiad-78d51b.netlify.app/",
+            },
+            {
+              name: "exercise2",
+              url: "https://deploy-preview-3--clever-naiad-78d51b.netlify.app/",
+            },
+            {
+              name: "exercise3",
+              url: "https://deploy-preview-4--clever-naiad-78d51b.netlify.app/",
+            },
+            {
+              name: "exercise4",
+              url: "https://exercise1-hello-world--clever-naiad-78d51b.netlify.app/",
+            },
+            {
+              name: "exercise5",
+              url: "https://exercise1-hello-world--clever-naiad-78d51b.netlify.app/",
+            },
+            {
+              name: "exercise6",
+              url: "https://exercise1-hello-world--clever-naiad-78d51b.netlify.app/",
+            },
+            {
+              name: "exercise7",
+              url: "https://exercise1-hello-world--clever-naiad-78d51b.netlify.app/",
+            },
+            {
+              name: "exercise8",
+              url: "https://exercise1-hello-world--clever-naiad-78d51b.netlify.app/",
+            },
+            {
+              name: "exercise9",
+              url: "https://exercise1-hello-world--clever-naiad-78d51b.netlify.app/",
+            },
+          ].map((link, index) => (
+            <li key={index}>
+              <a href={link.url}>{link.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form className="form">
+        <div>
+          <input
+            type="text"
+            placeholder="First name"
+            className="form-input"
+            onChange={(e) => setFirstName(e?.target?.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Last name"
+            className="form-input"
+            onChange={(e) => setLastName(e?.target?.value)}
+          />
+        </div>
+        <button type="button" onClick={handleFormSubmit} className="form-btn">
+          Greet me
+        </button>
+      </form>
     </div>
   );
 }
